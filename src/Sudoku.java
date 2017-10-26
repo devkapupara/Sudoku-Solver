133306a50adcfcf9f51c259fd16bf3c5d974f162
itchiTriggah <itchiTriggah@gmail.com>
2017-10-25 22:10:31 -0700

    Solves any NxN puzzle

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -10,10 +10,36 @@ import java.util.Scanner;
 public class Sudoku
 {
     public int[][] puz;
+    private int size;
+    private int box_r;
+    private int box_c;
 
     public Sudoku()
     {
-        puz = new int[9][9];
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter size of sudoku: ");
+        size = s.nextInt();
+        System.out.print("Enter # rows of inner box: ");
+        box_r = s.nextInt();
+        System.out.print("Enter # columns of inner box: ");
+        box_c = s.nextInt();
+        s.close();
+        puz = new int[size][size];
+    }
+
+    public int length()
+    {
+        return puz.length;
+    }
+
+    public int getBox_r()
+    {
+        return box_r;
+    }
+
+    public int getBox_c()
+    {
+        return box_c;
     }
 
     public boolean loadSudoku()
@@ -24,13 +50,14 @@ public class Sudoku
 
             if (!scan.hasNextLine())    {System.out.println("Empty File. Exiting..."); return false;}
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < puz.length; i++)
             {
-                for (int j = 0; j < 9; j++)
+                for (int j = 0; j < puz[i].length; j++)
                 {
                     puz[i][j] = scan.nextInt();
                 }
             }
+            scan.close();
         }
         catch(FileNotFoundException e)
         {
@@ -42,16 +69,27 @@ public class Sudoku
 
     public void printSudoku()
     {
+        int r = 0;
+        int c = 0;
+        String repeatedStar = new String(new char[4*puz.length+4]).replace('\0', '-');
         for (int[] i: puz)
         {
-            System.out.println("-------------------------------------");
+            if (r % box_r == 0)
+                System.out.print(repeatedStar+'\n');
             for(int j: i)
             {
-                System.out.print("| " + j + " ");
+                if (c % box_c == 0)
+                    System.out.print('|');
+                if (j < 10)
+                    System.out.print("  " + j + " ");
+                else
+                    System.out.print(" " + j + " ");
+                c++;
             }
+            r++;
             System.out.print("|");
             System.out.println();
         }
-        System.out.println("-------------------------------------");
+        System.out.print(repeatedStar+'\n');
     }
 }
--- src/solvePuzzle.java
+++ src/solvePuzzle.java
@@ -1,30 +1,40 @@
 package Sudoku;
 
+import java.util.Scanner;
+
 /**
  * Created by Jarvis on 9/23/17.
  */
 public class solvePuzzle
 {
-    static Sudoku sud = new Sudoku();
+    private Sudoku sud;
+    private int size;
 
-    public static boolean solvePuzzle(int row, int col)
+
+    public solvePuzzle()
     {
-        if (col == 9) {                                     // If column is 9th, we make it 0 and increase row number
+        sud = new Sudoku();
+        size = sud.length();
+    }
+
+    public boolean solve(int row, int col)
+    {
+        if (col == size) {                                  // If column is 9th, we make it 0 and increase row number
             col = 0;                                        // If row becomes 9, that means we are in the last cell
             row+=1;                                         // and our work is done so we return true.
-            if (row == 9) {
+            if (row == size) {
                 return true;
             }
         }
 
-        for (int num = 1; num < 10; num++)                  // For numbers between 1 to 9, we start picking up values.
+        for (int num = 1; num < size+1; num++)              // For numbers between 1 to 9, we start picking up values.
         {
             if (sud.puz[row][col] == 0)                     // If the entry in our matrix[row][col] is 0, that means its
             {                                               // empty and we try to fill it with the above value.
                 if (isLegal(row, col, num))                 // If that value is legal, fill it and the call the function
                 {                                           // again on the next cell.
                     sud.puz[row][col] = num;
-                    if (solvePuzzle(row, col + 1))      // We induce a recursive method that tries to fill each cell
+                    if (solve(row, col + 1))            // We induce a recursive method that tries to fill each cell
                     {                                       // with a valid number till we reach the end. If no value in
                         return true;                        // that cell is legal, our function returns false which
                     }                                       // transfers control to the recursive call of the previous
@@ -35,26 +45,27 @@ public class solvePuzzle
                 }                                           // which collapses all previous calls in one shot and returns
             }                                               // true.
             else
-                return solvePuzzle(row, col + 1);       // If that cell is not empty, skip it.
+                return solve(row, col + 1);             // If that cell is not empty, skip it.
         }
         return false;                                       // No solution was found therefore return false.
     }
-                // We Looped through all the cells and were able to fill// it out. So we return true.
+                                                            // We Looped through all the cells and were able to fill
+                                                            //it out. So we return true.
 
-    public static boolean isLegal(int row, int col, int num)
+    public boolean isLegal(int row, int col, int num)
     {
-        for (int j = 0; j < 9; j++)                             // Check for same number in row
+        for (int j = 0; j < size; j++)                      // Check for same number in row
         {
             if (sud.puz[row][j] == num) {return false;}
         }
 
-        for (int r = 0; r < 9; r++)                             // Check for same number in column
+        for (int r = 0; r < size; r++)                      // Check for same number in column
         {
             if (sud.puz[r][col] == num) {return false;}
         }
 
-        int topRightRow = (row / 3) * 3;                        // Check for same number in that box.
-        int topRightCol = (col / 3) * 3;
+        int topRightRow = (row / sud.getBox_r()) * sud.getBox_r();           // Check for same number in that box.
+        int topRightCol = (col / sud.getBox_c()) * sud.getBox_c();
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
@@ -70,12 +81,13 @@ public class solvePuzzle
 
     public static void main(String[] args)
     {
-        if(sud.loadSudoku() == false)   {return;}
+        solvePuzzle sp = new solvePuzzle();
+        if(!sp.sud.loadSudoku())   {return;}
         long start = System.currentTimeMillis();
-        if(solvePuzzle(0,0)) {
-            System.out.println("Solution found!");
-            sud.printSudoku();
-            System.out.println("It took me " + (System.currentTimeMillis() - start) + "ms to solve this puzzle.");
+        if(sp.solve(0,0)) {
+            System.out.println("Solution found!\n");
+            sp.sud.printSudoku();
+            System.out.println("\nIt took me " + (System.currentTimeMillis() - start) + "ms to solve this puzzle.");
         }
         else
         {
